d6403964eecc9f078deda0ff64aeeceb1f621a02
Cassandra Comar <cass@ndra.io>
2025-08-27 14:10:26 -0400

    revert

--- src/main.rs
+++ src/main.rs
@@ -503,13 +503,7 @@ async fn config() {
     input::keybind(mod_key | Mod::SHIFT, Keysym::Return)
         .on_press(|| {
             Command::new("/etc/profiles/per-user/cassandra/bin/emacsclient")
-                .args([
-                    "-c",
-                    "-F",
-                    "'(name . \"emacsclient\" fullscreen . 'fullheight))",
-                    "-e",
-                    "(+eat/here)",
-                ])
+                .args(["-c", "-F", "'(name . \"emacsclient\")", "-e", "(+eat/here)"])
                 .spawn();
         })
         .group("Process")
