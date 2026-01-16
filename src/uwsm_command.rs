de91a93f34c46a015e40096f3213a8a8a6e48bf9
Cassandra Comar <ccomar@drwholdings.com>
2026-01-16 13:13:10 -0500

    add the rest of the command fields

--- src/uwsm_command.rs
+++ src/uwsm_command.rs
@@ -1,9 +1,16 @@
+use std::collections::HashMap;
+
 use pinnacle_api::process::{Child, Command};
 
 pub struct UwsmCommand {
     command: String,
     args: Vec<String>,
     once: bool,
+    envs: HashMap<String, String>,
+    unique: bool,
+    pipe_stdin: bool,
+    pipe_stdout: bool,
+    pipe_stderr: bool,
 }
 
 impl UwsmCommand {
@@ -12,6 +19,11 @@ impl UwsmCommand {
             command: command.to_string(),
             args: Vec::new(),
             once: false,
+            envs: HashMap::new(),
+            unique: false,
+            pipe_stdin: false,
+            pipe_stdout: false,
+            pipe_stderr: false,
         }
     }
 
@@ -27,11 +39,50 @@ impl UwsmCommand {
     }
 
     pub fn spawn(self) -> Option<Child> {
-        if self.once {
-            Command::from(self).once().spawn()
-        } else {
-            Command::from(self).spawn()
-        }
+        Command::from(self).spawn()
+    }
+
+    pub fn arg(&mut self, arg: impl ToString) -> &mut Self {
+        self.args.push(arg.to_string());
+        self
+    }
+
+    pub fn env(&mut self, key: impl ToString, value: impl ToString) -> &mut Self {
+        self.envs.insert(key.to_string(), value.to_string());
+        self
+    }
+
+    pub fn envs<I, K, V>(&mut self, vars: I) -> &mut Self
+    where
+        I: IntoIterator<Item = (K, V)>,
+        K: ToString,
+        V: ToString,
+    {
+        self.envs.extend(
+            vars.into_iter()
+                .map(|(k, v)| (k.to_string(), v.to_string())),
+        );
+        self
+    }
+
+    pub fn unique(&mut self) -> &mut Self {
+        self.unique = true;
+        self
+    }
+
+    pub fn pipe_stdin(&mut self) -> &mut Self {
+        self.pipe_stdin = true;
+        self
+    }
+
+    pub fn pipe_stdout(&mut self) -> &mut Self {
+        self.pipe_stdout = true;
+        self
+    }
+
+    pub fn pipe_stderr(&mut self) -> &mut Self {
+        self.pipe_stderr = true;
+        self
     }
 }
 
@@ -39,9 +90,22 @@ impl From<UwsmCommand> for Command {
     fn from(value: UwsmCommand) -> Self {
         let mut cmd = Command::with_shell(["uwsm", "app", "-a", &value.command], &value.command);
         cmd.args(value.args);
+        cmd.envs(value.envs);
         if value.once {
             cmd.once();
         };
+        if value.unique {
+            cmd.unique();
+        }
+        if value.pipe_stdin {
+            cmd.pipe_stdin();
+        }
+        if value.pipe_stdout {
+            cmd.pipe_stdout();
+        }
+        if value.pipe_stderr {
+            cmd.pipe_stderr();
+        }
         cmd
     }
 }
